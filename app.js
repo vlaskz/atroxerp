ed25f8aff4cd8a68d3ef8bd599ee05fafa9e81c5
Isaias Velasquez <sam.velasquez@hotmail.com>
2020-10-14 15:01:05 -0300

    added /login route and some satic files

--- app.js
+++ app.js
@@ -2,10 +2,22 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const port = process.env.PORT || 3000
+const bodyParser = require('body-parser')
+const { static } = require('express')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static('./www/'))
 
 app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname+'/www/index.html'))
 })
+
+app.post('/login',(req, res)=>{
+    console.log(`Say hello to ${req.body.username}\nwith password ${req.body.password}`)
+    res.sendStatus(200);
+})
+
 
 app.listen(port, ()=>{
     console.log(`Running wild at ${port}`)
