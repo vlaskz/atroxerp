8f800ea7881c96b6e8663bfbe195930ee699c915
Isaias Velasquez <sam.velasquez@hotmail.com>
2020-10-13 22:37:09 -0300

    added some port possibilities

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname+'/index.html'))
 })
 
-app.listen(80, ()=>{
+app.listen(process.env.PORT||3000, ()=>{
     console.log(`Running wild`)
 })
