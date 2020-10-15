e97515126c487e61ac8eb5c2eeb0162c030258dc
Isaias Velasquez <sam.velasquez@hotmail.com>
2020-10-14 22:30:39 -0300

    added dashboard html file and material design 4 bs

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path')
 const port = process.env.PORT || 3000
 const bodyParser = require('body-parser')
 const { static } = require('express')
+const format = require('date-fns/format')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
@@ -15,7 +16,12 @@ app.get('/', (req,res)=>{
 
 app.post('/login',(req, res)=>{
     console.log(`Say hello to ${req.body.usermail}\nwith password ${req.body.password}`)
-    res.sendStatus(200);
+    res.json({
+        username: req.body.usermail,
+        password: req.body.password,
+        date: format(Date.now(), 'dd/MM/yyyy')
+    })
+    res.sendFile(path.join(__dirname+'/www/dashboard.html'))
 })
 
 
