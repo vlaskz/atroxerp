e6b852dcc4877f2fe50628fa582e649f105bc189
Isaias Velasquez <sam.velasquez@hotmail.com>
2020-10-14 22:42:29 -0300

    + mdb style for all pages

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express()
 const path = require('path')
 const port = process.env.PORT || 3000
 const bodyParser = require('body-parser')
-const { static } = require('express')
 const format = require('date-fns/format')
 
 app.use(bodyParser.json())
@@ -14,13 +13,17 @@ app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname+'/www/index.html'))
 })
 
-app.post('/login',(req, res)=>{
-    console.log(`Say hello to ${req.body.usermail}\nwith password ${req.body.password}`)
+app.post('/login_api',(req, res)=>{
+    console.log(`Returning a JSON response to ${req.body.usermail}`)
     res.json({
         username: req.body.usermail,
         password: req.body.password,
         date: format(Date.now(), 'dd/MM/yyyy')
     })
+})
+
+app.post('/login',(req,res)=>{
+    console.log(`Logon attempt from ${req.body.usermail} in ${format(Date.now(), 'dd.MM.yyyy hh:mm:ss')}.`)
     res.sendFile(path.join(__dirname+'/www/dashboard.html'))
 })
 
